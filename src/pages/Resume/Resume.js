04bfbec1850aecdf89a1b0d959db2d99b41eaea8
AKHIL <akhilkrishna36912@gmail.com>
2025-02-03 11:50:54 +0530

    added SoftSkill.js and it's contents in pages/Resume/components

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
-import './Resume.css';
-import Skills from './components/Skills';
-import Education from './components/Education';
+import React, { useEffect } from "react";
+import "./Resume.css";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import SoftSkills from "./components/SoftSkills";
 
 const Resume = () => {
-  useEffect(() => {
-      window.scroll(0,0)
-  },[])
-  return (
-    <main>
-        <h1>Resume</h1>
-        <Skills />
-        <Education />
-    </main>
-  )
-}
+    useEffect(() => {
+        window.scroll(0, 0);
+    }, []);
+    return (
+        <main>
+            <h1>Resume</h1>
+            <Skills />
+            <SoftSkills />
+            <Education />
+        </main>
+    );
+};
 
-export default Resume
+export default Resume;
---
+++ src/pages/Resume/components/SoftSkills.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SoftSkills = () => {
+    return (
+        <div className="resume-container">
+            <div className="left-section">
+                <h2 className="section-title">Soft Skills</h2>
+            </div>
+            <div className="right-section">
+                <div className="section-description">
+                    <ul>
+                        <li>Continuous learning</li>
+                        <li>Self learning</li>
+                        <li>Adaptability</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SoftSkills;
