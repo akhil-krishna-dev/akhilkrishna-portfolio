b6050ab4f1b624f37a971588323e98144c468053
AKHIL <akhilkrishna36912@gmail.com>
2025-02-03 12:46:52 +0530

    in Home.js about me section content changed

--- src/pages/Home/components/AboutMe.js
+++ src/pages/Home/components/AboutMe.js
@@ -1,20 +1,23 @@
-import './AboutMe.css';
+import "./AboutMe.css";
 
 const AboutMe = () => {
     return (
         <>
-        <div className="hello">Hello</div>
-        <h3>A Bit About Me</h3>
-        <p>
-            I'm a web developer <br/> specialiazed in <span className="pyth">Pyt</span>
-            <span className="dj">hon </span>
-            <span className="ngo">Django</span>{" "}
-            <span className="react">
-                React <span className="js">JS.</span>
-            </span>
-        </p>
+            <div className="hello">Hello</div>
+            <h3>A Bit About Me</h3>
+            <p>
+                Self-taught Full-Stack Developer with hands
+                <br /> on experience in{" "}
+                <strong className="hands-on-skills">
+                    Python, Django and React
+                </strong>
+                ,
+                <br /> building scalable web applications. Passionate <br />
+                about problem-solving and continuously
+                <br /> learning modern web technologies
+            </p>
         </>
-    )
-}
+    );
+};
 
-export default AboutMe
+export default AboutMe;
