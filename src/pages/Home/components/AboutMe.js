53acb688b2d44bf57c4cea09c92c18e400ff1891
akhil-krishna-dev <akhilkrishna36912@gmail.com>
2026-01-11 21:35:37 +0530

    Update in About me section.

--- src/pages/Home/components/AboutMe.js
+++ src/pages/Home/components/AboutMe.js
@@ -1,23 +1,24 @@
 import "./AboutMe.css";
 
 const AboutMe = () => {
-    return (
-        <>
-            <div className="hello">Hello</div>
-            <h3>A Bit About Me</h3>
-            <p>
-                Self-taught Full-Stack Developer with hands
-                <br /> on experience in{" "}
-                <strong className="hands-on-skills">
-                    Python, Django and React
-                </strong>
-                ,
-                <br /> building scalable web applications. Passionate <br />
-                about problem-solving and continuously
-                <br /> learning modern web technologies
-            </p>
-        </>
-    );
+	return (
+		<>
+			<div className="hello">Hello</div>
+			<h3>A Bit About Me</h3>
+			<p>
+				Self-taught Full-Stack Developer <br />
+				with hands on experience in <br />
+				<strong className="hands-on-skills">
+					Python & Django, Node.js & Express.js,
+					<br /> React & React Native
+				</strong>
+				,
+				<br /> building scalable applications. Passionate <br />
+				about problem-solving and continuously
+				<br /> learning modern web technologies
+			</p>
+		</>
+	);
 };
 
 export default AboutMe;
